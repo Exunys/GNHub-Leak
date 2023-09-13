2c5d4b14c28365ba74c3cd303c5d16ce61ee1a6f
Aleksandar <aleksandar.exunys@gmail.com>
2023-09-13 02:49:52 +0200

    Add files via upload

---
+++ GNHub Leak/Scripts/Loader.lua
@@ -0,0 +1,20 @@
+getfenv().lelBallz = false; function GNHubLoder() if L == 'GNHub' and getfenv().ProtectedGNHub == true then if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+if game.PlaceId == 4639625707 then
+    loadstring(game:HttpGetAsync("http://45.146.255.5:8100/War8834839T39oon.lua"))()
+elseif game.PlaceId == 286090429 then
+    loadstring(game:HttpGetAsync("http://45.146.255.5:8100/A42n134l.lua"))()
+else
+    loadstring(game:HttpGetAsync("http://45.146.255.5:8100/Uni377629a23l.lua"))()
+end
+
+local UserInputService = game:GetService("UserInputService")
+
+if not UserInputService.TouchEnabled then
+    loadstring(game:HttpGet("http://45.146.255.5:8100/UWPFPSBooster.lua"))()
+end getfenv().ProtectedGNHub = false
+ else
+    while true do print('GNHub ERROR') end
+ end end GNHubLoder();
---
+++ GNHub Leak/Scripts/Starting point.lua
@@ -0,0 +1,6 @@
+getfenv().lelBallz = false; function GNHubLoder() if L == 'GNHub' and getfenv().ProtectedGNHub == true then
+loadstring(game:HttpGetAsync("http://45.146.255.5:8100/Loader.lua"))() getfenv().ProtectedaNHub = false
+
+else
+while true do print('GNHub ERROR’) end
+end end GNHubLoder();
